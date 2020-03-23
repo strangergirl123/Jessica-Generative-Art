c2d7e3d350f046a7b3a497cca47b9d400b2c2f98
Jessica Lu <jl10698@nyu.edu>
2020-03-23 19:54:23 -0400

    updated project

--- sketch.js
+++ sketch.js
@@ -1,5 +1,94 @@
+timer=20;
+
+let isLoopActive=true;
+let width= 600
+let height=600
+
+function centerCanvas() {
+    var x = ((windowWidth - width) / 2)+100;
+    var y = ((windowHeight - height) / 2)+50;
+    cnv.position(x, y);
+  }
+  
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    fill("black");
-    rect(0, 0, width, height);
+    cnv= createCanvas(width, height);
+    background('white');
+    centerCanvas();
 }
+
+
+function draw(){
+    
+    noStroke();
+    fill(random(255), random(255), random(255), random(255));
+    ellipse(random(windowWidth), random(windowHeight), random(100));
+    timePassed();
+}
+
+function timePassed(){
+    print(timer);
+    if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
+        timer --;
+      }
+
+      else if (timer== 15) {
+        textSize(30);
+        fill('black');
+        let text1= text("Have you tried clicking?", 60, 80);
+        text1;
+      }
+
+
+      else if (timer == 11) {
+        textSize(30);
+        fill('black');
+        let text2= text("What about dragging?", width/2, height*0.7);
+        text2;
+      }
+
+    }
+  
+ function heart(x,y,size) {
+    beginShape();
+    vertex(x, y);
+    bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x, y +     size);
+    bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
+    endShape(CLOSE);
+  }
+
+function mouseDragged() {
+  fill('black');
+  ellipse(mouseX, mouseY, 20, 20);
+  // prevent default
+  return false;
+}
+
+function mousePressed() {
+    fill(255,25,25);
+    heart(mouseX, mouseY, random(100));
+  }
+
+  function keyPressed() {
+  if(keyCode==84){
+    clear();
+    background('white');
+    timer=1000000000000000000000000000;
+  }
+    
+    if(keyCode==32){
+    if(isLoopActive===true){
+      noLoop();
+      isLoopActive=false;
+    }
+    else{
+      loop();
+      isLoopActive=true;
+    }
+  }  
+
+}
+
+    
+  
+
